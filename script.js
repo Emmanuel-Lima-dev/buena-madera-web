76fcd63fa0cf8a28e3101db5b6ce52e321ccfe61
Emmanuel-Lima-dev <lima_kpo@hotmail.com>
2025-04-02 22:07:55 -0300

    decimo commit

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function isElementInViewport(el) {
     return (
         rect.top >= 0 &&
         rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) + 100 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
 }

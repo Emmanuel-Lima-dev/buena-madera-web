8f7dabf5124fa8de57d3544c5dd1b2a5874ac9a9
Emmanuel-Lima-dev <lima_kpo@hotmail.com>
2025-04-02 21:42:32 -0300

    noveno commit

--- script.js
+++ script.js
@@ -11,3 +11,27 @@ navbarCollapse.addEventListener("hidden.bs.collapse", () => {
     main.classList.remove("opacidad");
 });
 
+function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) + 100 &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+function handleScroll() {
+    const elements = document.querySelectorAll('.watch'); // Selecciona los elementos a observar
+    elements.forEach(el => {
+        if (isElementInViewport(el)) {
+            el.classList.add('animate__animated', 'animate__fadeInUp'); // Agrega la clase si el elemento está en la vista
+        } 
+    });
+}
+
+// Agrega el evento de scroll para detectar los elementos visibles
+document.addEventListener('scroll', handleScroll);
+
+// Llamar una vez al inicio para detectar elementos ya visibles
+handleScroll();
